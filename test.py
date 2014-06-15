5b0525f87faaf40044a93281a3e6af68ad7c9c80
Zhuoli <liang.zhu@husky.neu.edu>
2014-06-15 00:21:16 -0400

    done 2

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print 'hello world'
+import filecmp
+
+plaintext_files=['./tony/tony.txt','./alice/alice.txt']
+decrypted_files=['./alice/tony_decr.txt','./tony/alice_decr.txt']
+
+print 'Auto Test Script, Running...............'
+for i in range(0,len(plaintext_files)):
+  if not filecmp.cmp(plaintext_files[i],decrypted_files[i]):
+    print 'Decrypted File Not match: ' + plaintext_files[i] + "  " + decrypted_files[i]
+print "Decrypted Files All match"
